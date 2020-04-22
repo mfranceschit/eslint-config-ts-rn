14752d188e427d868f0d416b470524cd242d0efe
Marco Franceschi <mfranceschi@talpor.com>
2020-04-22 19:10:29 -0400

    Updated preferred rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,56 @@ module.exports = {
     "sourceType": "module"
   },
   "rules": {
-    "no-console": 0
+    "no-console": 2,
+    "react/display-name": 0,
+    "react/prop-types": 0,
+    "react/jsx-no-bind": [
+      "error",
+      {
+        "allowArrowFunctions": true,
+        "allowFunctions": true
+      }
+    ],
+    "react-native/no-color-literals": 0,
+    "react-native/no-inline-styles": 0,
+    "no-var": 2,
+    "arrow-parens": ["error", "as-needed"],
+    "no-console": 2,
+    "max-len": ["error", { "code": 140 }],
+    "@typescript-eslint/no-namespace": "error",
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/ban-ts-ignore": 0,
+    "no-unsafe-finally": 2,
+    "object-literal-sort-keys": 0,
+    "no-submodule-imports": 0,
+    "jsx-no-multiline-js": 0,
+    "comma-dangle": [
+      "error",
+      {
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "never",
+        "exports": "never",
+        "functions": "never"
+      }
+    ],
+    "@typescript-eslint/interface-name-prefix": [
+      "error",
+      {
+        "allowUnderscorePrefix": false,
+        "prefixWithI": "always"
+      }
+    ],
+    "sort-imports": [
+      "error",
+      {
+        "ignoreCase": false,
+        "ignoreDeclarationSort": true,
+        "ignoreMemberSort": false,
+        "memberSyntaxSortOrder": ["none", "all", "multiple", "single"]
+      }
+    ],
+    "no-unused-expressions": ["error", { "allowShortCircuit": true, "allowTernary": true }]
   },
   "plugins": ["prettier", "react-hooks"],
   "settings": {
